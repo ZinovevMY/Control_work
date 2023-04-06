5cefb2ed8fc4736b5d1fc3ec03c99e935cba95ee
Mikhail <mehan@list.ru>
2023-04-06 21:02:31 +0300

    notemapper added

---
+++ model/Notemapper.py
@@ -0,0 +1,15 @@
+from model.Note import Note
+
+
+class Notemapper:
+
+    def __init__(self):
+        pass
+
+    def load_map(self, line:list):
+        note = Note(id=line[0], header=line[1],text=line[2],date=line[3])
+        return note
+
+    def save_map(self, note:Note):
+        file_record = [note.get_id(), note.get_header(), note.get_text(), note.get_date()]
+        return file_record
